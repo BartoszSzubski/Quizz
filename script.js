ccc037d34f1c31bb4fa5464a4cd23ec4267d6e01
BartoszSzubski <szubscy@gmail.com>
2024-01-15 16:37:53 +0100

    html and scss, working on js

---
+++ script.js
@@ -0,0 +1,47 @@
+const questions = [
+  {
+    question: "Why?",
+    answers: [
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+    ],
+  },
+  {
+    question: "Why?",
+    answers: [
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+    ],
+  },
+  {
+    question: "Why?",
+    answers: [
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+    ],
+  },
+  {
+    question: "Why?",
+    answers: [
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+    ],
+  },
+  {
+    question: "Why?",
+    answers: [
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+      { text: "smth", correct: false },
+    ],
+  },
+];
